026e2e339547d646857c977baeac5223b51127ac
0x1000000 <25109784+0x1000000@users.noreply.github.com>
2021-03-07 13:50:57 -0800

    fixes running pack.sh on Linux

--- pack.sh
+++ pack.sh
@@ -9,9 +9,9 @@ OUTDIRPACK="$ROOT/package"
 rm -rf "$ROOT/tmp"
 rm -rf "$OUTDIRPACK"
 
-dotnet test "$ROOT/Test/SqExpress.Test/SqExpress.Test.csproj"
+dotnet test "$ROOT/Test/SqExpress.Test/SqExpress.Test.csproj" -f "netcoreapp3.1"
 
-dotnet build "$ROOT/SqExpress.CodegenUtil/SqExpress.CodegenUtil.csproj" -o "$OUTDIRTOOLS" -c Release
+dotnet build "$ROOT/SqExpress.CodegenUtil/SqExpress.CodeGenUtil.csproj" -o "$OUTDIRTOOLS" -c Release
 
 rm -rf "$OUTDIRTOOLS"/*.dev.json
 
